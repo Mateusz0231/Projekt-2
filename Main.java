8e0a57e3ea8d1c04a9d4a2001365bb7c1683cd38
Mateusz <100692029+Mateusz0231@users.noreply.github.com>
2022-06-24 16:41:07 +0000

    Działa silnia ale nie działa szukanie

--- Main.java
+++ Main.java
@@ -11,12 +11,36 @@ class Main {
     return s;
     
   }
-  public static void main(String[] ) {
+  public static void main(String[] args) {
    System.out.println("Podaj dodatnią liczbę całkowitą");
     Scanner skaner = new Scanner(System.in);
     int n= skaner.nextInt();
     System.out.printf("Silnia z %d wynosi %d",n,silnia(n));
     
-   
+
+    //Wyznaczenie silni działa prawidłowo do 20 
+ //Poniżej jest program na wyszukanie wartości który niestety nie zadziałał
+    
+   // Scanner a =new Scanner(System.in);
+   //int t = a.nextInt();
+  
+   // for (int i = 0; i<t; i++) {
+       
+        //Scanner b = new Scanner(System.in);
+        // int liczba = a.nextInt();
+      
+        // s = obliczSilnia(liczba);
+ 
+        
+       // while (silnia/10*10==silnia) {
+      //      silnia /= 10;
+       // }
+ 
+        
+ //       System.out.println(silnia%10);
+  //  }
+ 
+    
+    
   }
 }
